3cc7f939ffbb1e4df2f55ecc91355af95ad92ff5
corradofadda <corradofadda@users.noreply.github.com>
2020-06-05 10:47:04 +0200

    Preprocessing

---
+++ resnet50_transfer.m
@@ -0,0 +1,51 @@
+clear
+clc
+
+%CNN-Based Image Analysis for Malaria Detection
+
+% Acquisizione del Data Set
+malaria_ds = imageDatastore('C:\Users\Corrado\Documents\Informatica\. Tesi\preprocessed_cell_images','IncludeSubfolders',true,'LabelSource','foldernames');
+[trainImgs, testImgs, validImgs] = splitEachLabel(malaria_ds,0.80,0.10,0.10,'randomized');
+
+%Augmentation
+imageAugmenter = imageDataAugmenter('RandRotation',[-60,60],'RandXTranslation',[-3 3],'RandYTranslation',[-3 3],'RandXReflection',true,'RandYReflection',true);
+
+trainImgs_a = augmentedImageDatastore([224 224], trainImgs,'ColorPreprocessing','gray2rgb','DataAugmentation',imageAugmenter);
+validImgs_a = augmentedImageDatastore([224 224], validImgs,'ColorPreprocessing','gray2rgb');
+testImgs_a = augmentedImageDatastore([224 224], testImgs,'ColorPreprocessing','gray2rgb');
+
+%Modifica di Resnet50
+a = fullyConnectedLayer(2,'Name','FCL');
+b = classificationLayer('Name','fine');
+lgraph = layerGraph(resnet50);
+lgraph = replaceLayer(lgraph,'fc1000',a);
+lgraph = replaceLayer(lgraph,'ClassificationLayer_fc1000',b);
+
+
+% Opzioni del Training
+options = trainingOptions('adam',...
+    'InitialLearnRate',1e-4,...
+    'MiniBatchSize',8,...
+    'ValidationData',validImgs_a,...
+    'ValidationFrequency',800,...
+    'ValidationPatience',5,...
+    'MaxEpochs',10,...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.1, ...
+    'LearnRateDropPeriod',8, ...   
+    'Plots','training-progress',...    
+    'Shuffle','every-epoch',...
+    'Verbose',false,...
+    'ExecutionEnvironment','gpu');
+
+% Training
+[malarianet,info] = trainNetwork(trainImgs_a,lgraph,options);
+
+% Classificazione delle immagini
+testpreds = classify(malarianet,testImgs_a);
+
+% Validazione della rete
+truetest = testImgs.Labels;
+numCorrect = nnz(testpreds == truetest);
+fracCorrect = numCorrect / numel(testpreds);
+confusionchart(truetest,testpreds);
